10c59da620b6a86335223354096ee4e32e45cd75
Andrew <shepelev.andrew.101@gmail.com>
2023-03-17 21:13:00 +0200

    added new view model

---
+++ NMT_Counter/Models/SubjectsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NMT_Counter.Models;
+public enum SubjectsViewModel
+{
+    [Display(Name = "Українська мова")]
+    Ukrainian,
+    [Display(Name = "Математика")]
+    Math,
+    [Display(Name = "Історія України")]
+    History,
+    [Display(Name = "Англійська мова")]
+    English,
+    [Display(Name = "Фізика")]
+    Physics,
+    [Display(Name = "Біологія")]
+    Biology,
+    [Display(Name = "Хімія")]
+    Chemistry
+}
